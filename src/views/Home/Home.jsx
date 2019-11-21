e0dbd8dbe4bf148ff46b74df96fbe85dd1c235b0
arcmena <marcelomenadev@gmail.com>
2019-11-20 23:41:23 -0300

    Adding search by id function

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,15 +3,16 @@ import api from '../../services/api';
 import { Header, Footer } from '../../components';
 import './Home.css';
 import search_icon from '../../assets/search-icon.png';
-import Axios from 'axios';
 
 export default class Home extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            active: true,
+            active1: true,
             topTen: [],
-            searchOne: [],
+            coin: {},
             input: ''
         }
     }
@@ -28,14 +29,26 @@ export default class Home extends React.Component {
 
     handleButtonClick = () => {
         api.get(`/coin/${this.state.input}`)
-        .then(res =>{
+        .then(res => {
             this.setState({
-                searchOne: res.data
+                active1: false,
+                coin: res.data
+            });
+            this.setState({
+                active:true,
             });
         })
         .catch(error => console.error(error));
     }
-    
+
+    handleButtonClick1 = () => {
+        this.setState({
+            active: false,
+        });
+        this.setState({
+            active1:true,
+        });
+    }
 
     render() {
         return(
@@ -46,17 +59,16 @@ export default class Home extends React.Component {
                         <input onChange={event =>
                         this.setState({input:event.target.value})} type="text" placeholder="Search by ID"></input>
                         <button onClick={this.handleButtonClick} ><img src={search_icon} alt="search icon" id="search_icon"/></button>
-                        {this.state.searchOne.map((coin) => (
-                        <div className="coin">
-                            <p>{`Name: ${coin.name} - Price (USD): ${parseFloat(coin.price_usd).toFixed(2)}`}</p>
-                        </div>
-                        ))}
+                        <button onClick={this.handleButtonClick1}>Show top ten</button>
                     </div>
                     {this.state.topTen.map((coin, i) => (
-                        <div key={i} className="coin">
+                        <div key={i} style={{display: this.state.active ? 'none' : 'block'}} className="coin">
                             <p>{`${i + 1} - ${coin.name} - Price (USD): ${parseFloat(coin.price_usd).toFixed(2)}`}</p>
                         </div>
                     ))}
+                    <div style={{display: this.state.active1 ? 'none' : 'block'}} className="coin">
+                        <p>{`Name: ${this.state.coin.name} - Price (USD): ${parseFloat(this.state.coin.price_usd).toFixed(2)}`}</p>
+                    </div>
                     <Footer/>
                 </div>
             </div>
